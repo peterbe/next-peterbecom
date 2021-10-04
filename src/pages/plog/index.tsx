fdd017f4787ca3607e1f6b4ad3f6069eb9eb812a
Peter Bengtsson <mail@peterbe.com>
2021-10-04 13:27:51 -0400

    fix archives page

--- src/pages/plog/index.tsx
+++ src/pages/plog/index.tsx
@@ -1,21 +1,10 @@
-// import type { NextPage } from "next";
-// import Head from "next/head";
-import type { AppProps } from "next/app";
-import {
-  GetStaticProps,
-  GetStaticPaths,
-  GetServerSideProps,
-  InferGetStaticPropsType,
-  InferGetServerSidePropsType,
-} from "next";
-import { Fragment } from "react";
-import styles from "../../styles/Plog.module.css";
-import { Footer } from "../../components/footer";
+import { GetServerSideProps } from "next";
+
+import { Archive } from "../../components/plog/archive";
 
 interface Post {
   oid: string;
   title: string;
-  // id: string;
   categories: string[];
   comments: number;
 }
@@ -23,68 +12,30 @@ interface Group {
   date: string;
   posts: Post[];
 }
-interface Data {
+interface ServerData {
   groups: Group[];
 }
-// interface Groups {
-//   [key: string]: Post[];
-// }
-// interface Props {
-//   // children: ReactNode;
-//   groups: Groups;
-// }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const res = await fetch("http://localhost:8000/api/v1/plog/");
-  const data: Data = await res.json();
-  // const { groups } = data;
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
+export const getServerSideProps: GetServerSideProps = async () => {
+  const url = `${process.env.API_BASE}/api/v1/plog/`;
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`${res.status} on ${url}`);
+  }
+  const data: ServerData = await res.json();
+  const { groups } = data;
+
   return {
-    props: data,
+    props: {
+      groups,
+    },
   };
 };
 
-// const Plog: NextPage = ({ groups }: { groups: Group[] }) => {
-const Plog = ({ groups }: { groups: Group[] }) => {
-  // }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  // function Plog({ data }: InferGetStaticPropsType<typeof getServerSideProps>) {
-  // function Plog({ groups }: { groups: Group[] }) {
-  return (
-    <div className={styles.container}>
-      <h1>Archive</h1>
-      <dl>
-        {groups.map(({ date, posts }) => {
-          return (
-            <Fragment key={date}>
-              <dt>{date}</dt>
-              {posts.map((post) => {
-                return (
-                  <dd key={post.oid}>
-                    <a href={`/plog/${post.oid}`}>{post.title}</a>
-                    {post.comments > 0 && (
-                      <span className={styles.comments}>
-                        {post.comments.toLocaleString()} comment
-                        {post.comments === 1 ? "" : "s"}
-                      </span>
-                    )}
-                    <span className={styles.categories}>
-                      {post.categories.join(", ")}
-                    </span>
-                  </dd>
-                );
-              })}
-            </Fragment>
-          );
-        })}
-      </dl>
-      {/* <Head>
-        <title>About Peterbe.com</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head> */}
-      <Footer />
-    </div>
-  );
+type ViewProps = React.ComponentProps<typeof Archive>;
+
+const View = (props: ViewProps) => {
+  return <Archive {...props} />;
 };
 
-export default Plog;
+export default View;
