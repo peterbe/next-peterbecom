b525603a28bed9a7d67d16a162785f3a4f098ada
Peter Bengtsson <mail@peterbe.com>
2021-10-27 07:38:44 -0400

    use 127

--- src/lib/_constants.ts
+++ src/lib/_constants.ts
@@ -2,5 +2,5 @@ export const USE_GOOGLE_ANALYTICS = process.env.NODE_ENV !== "development";
 // export const USE_GOOGLE_ANALYTICS = true;
 export const GOOGLE_ANALYTICS_DEBUG = false;
 
-export const API_BASE = process.env.API_BASE || "http://localhost:8000";
+export const API_BASE = process.env.API_BASE || "http://127.0.0.1:8000";
 if (!API_BASE) throw new Error("$API_BASE must be set");
