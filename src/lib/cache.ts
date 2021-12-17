59c022722b50ef68e9c47b51efa227892886cb79
Peter Bengtsson <mail@peterbe.com>
2021-12-17 17:40:20 -0500

    only cache for 12h instead (#49)

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -2,7 +2,7 @@ import type { ServerResponse } from "http";
 
 export function cacheHeader(
   res: ServerResponse,
-  cacheControl = "public,max-age=86400"
+  cacheControl = "public,max-age=43200" // 12h
 ) {
   if (process.env.NODE_ENV !== "development") {
     res.setHeader("Cache-Control", cacheControl);
