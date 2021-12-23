7d8287e2c5eacf9cddd23b2d4a729a817baa546e
Peter Bengtsson <mail@peterbe.com>
2021-12-23 10:12:31 -0500

    rearrange

--- middleware/legacy-redirects.mjs
+++ middleware/legacy-redirects.mjs
@@ -4,6 +4,13 @@ export function legacyRedirects(req, res, next) {
     return res.redirect(301, req.path);
   }
 
+  // TODO: Consider to redirect all unknown query strings that aren't known.
+  if (req.query.magmadomain) {
+    // I don't know what these are or where they come from. But they
+    // bypass the CDN cache.
+    return res.redirect(301, req.path);
+  }
+
   // Maybe turn all sorts of other junk query strings into redirects
 
   return next();
