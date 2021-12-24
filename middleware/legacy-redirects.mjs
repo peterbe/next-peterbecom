16870b28360d54472b68cd6fe22446b6378e39d4
Peter Bengtsson <mail@peterbe.com>
2021-12-24 17:25:00 -0500

    redirect on querystring 'author' too

--- middleware/legacy-redirects.mjs
+++ middleware/legacy-redirects.mjs
@@ -5,7 +5,7 @@ export function legacyRedirects(req, res, next) {
   }
 
   // TODO: Consider to redirect all unknown query strings that aren't known.
-  if (req.query.magmadomain) {
+  if (req.query.magmadomain || req.query.author) {
     // I don't know what these are or where they come from. But they
     // bypass the CDN cache.
     return res.redirect(301, req.path);
