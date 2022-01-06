eb44be230485a0631d611ae9cb313049a7c541a9
Peter Bengtsson <mail@peterbe.com>
2022-01-06 11:29:40 -0500

    avoid CLS by giving the carbonads outer a min-height

--- src/components/carbonad.tsx
+++ src/components/carbonad.tsx
@@ -1,14 +1,17 @@
 export function CarbonAd() {
-  // if (process.env.NEXT_PUBLIC_NO_ADS) {
-  if (process.env.NODE_ENV === "development") {
-    return null;
-  }
+  // if (process.env.NODE_ENV === "development") {
+  //   return null;
+  // }
 
   return (
-    <script
-      async
-      src="https://cdn.carbonads.com/carbon.js?serve=CKYI52Q7&amp;placement=peterbecom"
-      id="_carbonads_js"
-    ></script>
+    // The outer wrapper is to be able to set a min-height so
+    // as to avoid a CLS when the ad loads in.
+    <div className="carbonads_outer">
+      <script
+        async
+        src="https://cdn.carbonads.com/carbon.js?serve=CKYI52Q7&amp;placement=peterbecom"
+        id="_carbonads_js"
+      ></script>
+    </div>
   );
 }
