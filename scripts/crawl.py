4dfea3838cbda1b3ed08c0b21b5f8d1dadf0487e
Peter Bengtsson <mail@peterbe.com>
2022-02-18 08:02:21 -0500

    black

--- scripts/crawl.py
+++ scripts/crawl.py
@@ -12,8 +12,9 @@ import requests
 def seconds(s):
     return f"{s * 1000:.1f}ms"
 
+
 def main(args):
-    r = requests.get('http://localhost:3000/sitemap.xml')
+    r = requests.get("http://localhost:3000/sitemap.xml")
     r.raise_for_status()
     if args:
         max_urls = int(args[0])
@@ -21,27 +22,32 @@ def main(args):
         max_urls = 0
 
     urls = []
-    for loc in re.findall('<loc>(.*?)</loc>', r.text):
+    for loc in re.findall("<loc>(.*?)</loc>", r.text):
         path = urlparse(loc).path
-        urls.append('http://localhost:3000' + path)
+        urls.append("http://localhost:3000" + path)
 
     import random
+
     random.shuffle(urls)
 
     times = defaultdict(list)
-    for i, url in enumerate(urls[:max_urls or 9999]):
+    for i, url in enumerate(urls[: max_urls or 9999]):
         t0 = time.time()
         r = requests.get(url)
         t1 = time.time()
         r.raise_for_status()
-        print(i+1, seconds(t1- t0).ljust(10), r.headers['x-middleware-cache'], url)
-        times[r.headers['x-middleware-cache']].append(t1 - t0)
+        print(i + 1, seconds(t1 - t0).ljust(10), r.headers["x-middleware-cache"], url)
+        times[r.headers["x-middleware-cache"]].append(t1 - t0)
+
     for cache_hit, numbers in times.items():
-        print(cache_hit.ljust(10), seconds(statistics.mean(numbers)))
+        print(
+            cache_hit.ljust(10),
+            seconds(statistics.mean(numbers)),
+            f"({len(numbers)} samples)",
+        )
 
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
+
     main(sys.argv[1:])
