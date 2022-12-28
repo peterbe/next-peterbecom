d685a9404d2b4140d7c0fef71f1ad249f6680f00
Peter Bengtsson <mail@peterbe.com>
2022-12-28 16:19:15 -0500

    tidying

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,19 @@
 import Head from "next/head";
-// import '../styles/globals.css'
 import "../styles/css/semantic/reset.min.css";
 import "../styles/css/semantic/site.min.css";
-// import "../styles/css/semantic/button.css",
 import "../styles/css/slim-semantic-button.css"; // My own!
 import "../styles/css/semantic/container.min.css";
-// import "../styles/css/semantic/divider.css",
 import "../styles/css/semantic/header.css";
 import "../styles/css/semantic/input.css"; // has custom hacks
-// import "../styles/css/semantic/label.css",
-// import "../styles/css/semantic/list.min.css";
-// import "../styles/css/semantic/loader.min.css";
 import "../styles/css/semantic/segment.min.css";
 import "../styles/css/semantic/form.min.css";
-// import "../styles/css/semantic/grid.css"
 import "../styles/css/slim-semantic-grid.css";
 import "../styles/css/semantic/menu.min.css";
 import "../styles/css/semantic/message.min.css";
 import "../styles/css/semantic/table.min.css";
 import "../styles/css/semantic/item.min.css";
 import "../styles/css/semantic/comment.min.css";
-// import "../styles/css/semantic/dimmer.min.css";
 import "../styles/css/semantic/dropdown.min.css";
-// import "../styles/css/semantic/search.css";
 import "../styles/css/highlight.css";
 import "../styles/css/peterbe.css";
 
